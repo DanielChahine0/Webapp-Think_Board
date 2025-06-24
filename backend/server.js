74243587c7b21b8196f4121ace64230b3b7c061c
DanielChahine0 <Chahinedaniel0@gmail.com>
2025-06-24 15:59:16 -0400

    moved the routes to a separate file for best practices

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,15 @@
 // I'm using the import because I changed package.json to have a module type ("type": "module",)
 import express from "express"
+import notesRoutes from "./routes/notesRoutes.js"
 
 // To run this we need to change the type of the package.json to commonjs ("type: "commonjs",)
 // const express = require("express");
 
 const app = express();
 
-// Routes
-app.get("/api/notes", (req,res) => {
-    res.status(200).send("you got 5 notes");
-})
+app.use("/api/notes", notesRoutes);
 
-app.post("/api/notes", (req,res) => {
-    res.status(201).json({message: "Note created successfully!"});
-})
-app.put("/api/notes/:id", (req,res) => {
-    res.status(200).json({message: "Note updated successfully!"});
-})
-app.delete("/api/notes/:id", (req,res) => {
-    res.status(200).json({message: "Note deleted successfully!"});
-})
+
 
 app.listen(5001, () => {
     console.log("Server started on PORT: 5001");
